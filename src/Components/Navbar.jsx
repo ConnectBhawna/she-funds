96c66733aee9cdf99512530e5bf2c6ed3aa0f8d2
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2022-09-03 20:35:00 +0530

    added login checks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,6 +10,7 @@ export default function NavBar({ pitchDesk, create }) {
   const [publicKey, setPublicKey] = useState("");
   const [dropdown, setDropdown] = useState(false);
   const [loadingLoginStatus, setLoadingLoginStatus] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     const loggedInKey = localStorage.getItem("loggedInKey");
     if (loggedInKey) {
@@ -74,68 +75,110 @@ export default function NavBar({ pitchDesk, create }) {
           </div>
         </div>
         {!loadingLoginStatus && (
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
-            }`}>
-            <ul className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
-              {pitchDesk && (
-                <li className='text-gray-600 hover:text-blue-600'>
-                  <Link to='/home'>Pitch Deck</Link>
-                </li>
-              )}
-              {create && (
-                <li className='text-gray-600 hover:text-blue-600'>
-                  <Link
-                    to='/pitch'
-                    className='bg-blue-500 text-white px-6 py-3 rounded-md shadow-sm hover:bg-blue-700'>
-                    Pitch <span className="mx-1 fas fa-pen"></span>
-                  </Link>
-                </li>
-              )}
-
-              {!hasLoggedIn && (
-                <li className='text-gray-600 hover:text-blue-600'>
-                  <button
-                    className='px-8 py-2 primaryColor text-white rounded-md shadow-md hover:shadow-xl'
-                    onClick={handleDesoLogin}>
-                    Login
-                  </button>
-                </li>
-              )}
-              {hasLoggedIn && (
-                <li className='text-gray-600 hover:text-blue-600 flex justify-center space-x-1 items-center'>
-                  <button
-                    id='dropdownDefault'
-                    data-dropdown-toggle='dropdown'
-                    onClick={() => setDropdown(!dropdown)}>
-                    <img
-                      src={`https://node.deso.org/api/v0/get-single-profile-picture/${publicKey}?fallback=https://diamondapp.com/assets/img/default_profile_pic.png`}
-                      className='w-9 h-9 rounded-full'
-                      alt='profile'
-                    />
-                  </button>
-
-                  {dropdown && (
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "block" : "hidden"
+              }`}>
+              <ul className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
+                {pitchDesk && (
+                  <li className='text-gray-600 hover:text-blue-600'>
+                    <Link to='/home'>Pitch Deck</Link>
+                  </li>
+                )}
+                {create && (
+                  <li className='text-gray-600 hover:text-blue-600'>
                     <button
-                    className='px-6 py-2  text-red-500 underline hover:text-red-700'
-                    onClick={() => {
-                      localStorage.removeItem("loggedInKey");
-                      window.location.reload();
-                    }}>
-                    Logout
-                  </button>
-                  )}
-                </li>
-              )}
+                      onClick={() => {
+                        if (hasLoggedIn) {
+                          window.location.href = "/create";
+                        } else {
+                          setShowModal(true);
+                        }
+                      }}
+                      className='bg-blue-500 text-white px-6 py-3 rounded-md shadow-sm hover:bg-blue-700'>
+                      Pitch <span className='mx-1 fas fa-pen'></span>
+                    </button>
+                  </li>
+                )}
 
-              
-            </ul>
+                {!hasLoggedIn && (
+                  <li className='text-gray-600 hover:text-blue-600'>
+                    <button
+                      className='px-8 py-2 primaryColor text-white rounded-md shadow-md hover:shadow-xl'
+                      onClick={handleDesoLogin}>
+                      Login
+                    </button>
+                  </li>
+                )}
+                {hasLoggedIn && (
+                  <li className='text-gray-600 hover:text-blue-600 flex justify-center space-x-1 items-center'>
+                    <button
+                      id='dropdownDefault'
+                      data-dropdown-toggle='dropdown'
+                      onClick={() => setDropdown(!dropdown)}>
+                      <img
+                        src={`https://node.deso.org/api/v0/get-single-profile-picture/${publicKey}?fallback=https://diamondapp.com/assets/img/default_profile_pic.png`}
+                        className='w-9 h-9 rounded-full'
+                        alt='profile'
+                      />
+                    </button>
+
+                    {dropdown && (
+                      <button
+                        className='px-6 py-2  text-red-500 underline hover:text-red-700'
+                        onClick={() => {
+                          localStorage.removeItem("loggedInKey");
+                          window.location.reload();
+                        }}>
+                        Logout
+                      </button>
+                    )}
+                  </li>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
         )}
       </div>
+
+      {showModal && !hasLoggedIn ? (
+        <>
+          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+            <div className='relative w-auto my-6 mx-auto max-w-3xl'>
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
+                  <h3 className='text-3xl font-semibold'>
+                    Deso Login Required
+                  </h3>
+                  <button
+                    className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                    onClick={() => setShowModal(false)}>
+                    <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className='relative p-6 flex-auto'>
+                  You need to login with Deso Identity to pitch your ideas and
+                  start fundraiser
+                </div>
+                {/*footer*/}
+                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                  <button
+                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                    type='button'
+                    onClick={() => setShowModal(false)}>
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+        </>
+      ) : null}
     </nav>
   );
 }
--- src/Components/Pitch.jsx/Pitch.jsx
+++ src/Components/Pitch.jsx/Pitch.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
 import NavBar from "../Navbar";
 
 export default function Pitch() {
+    useEffect(() => {
+        const loggedInKey = localStorage.getItem("loggedInKey");
+        if(!loggedInKey){
+            window.location.href = "/home";
+        }
+    }, [])
   return (
     <div>
       <NavBar pitchDesk={false} create={false} />
